7a53991605aae26db5009688100d68cfa14e4fd1
qyrn <contact@qyrn.dev>
2026-05-21 02:58:44 +0200

    ✨ feat: reproduce header brand lockup on the Open Graph image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -14,27 +14,29 @@ type LoadedFont = {
   style: "normal";
 };
 
-async function loadInter(): Promise<LoadedFont[] | undefined> {
+const FONT_UA = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)";
+
+async function fetchFontFiles(query: string): Promise<ArrayBuffer[]> {
+  const css = await fetch(`https://fonts.googleapis.com/css2?${query}`, {
+    headers: { "User-Agent": FONT_UA },
+  }).then((r) => r.text());
+  const urls = [
+    ...css.matchAll(/url\((https:\/\/[^)]+\.(?:ttf|otf|woff))\)/g),
+  ].map((m) => m[1]);
+  return Promise.all(urls.map((u) => fetch(u).then((r) => r.arrayBuffer())));
+}
+
+async function loadFonts(): Promise<LoadedFont[] | undefined> {
   try {
-    const css = await fetch(
-      "https://fonts.googleapis.com/css2?family=Inter:wght@400;800",
-      {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
-        },
-      },
-    ).then((r) => r.text());
-    const urls = [
-      ...css.matchAll(/url\((https:\/\/[^)]+\.(?:ttf|otf|woff))\)/g),
-    ].map((m) => m[1]);
-    if (urls.length < 2) return undefined;
-    const [regular, bold] = await Promise.all(
-      urls.slice(0, 2).map((u) => fetch(u).then((r) => r.arrayBuffer())),
-    );
+    const [inter, marker] = await Promise.all([
+      fetchFontFiles("family=Inter:wght@400;800"),
+      fetchFontFiles("family=Permanent+Marker"),
+    ]);
+    if (inter.length < 2 || marker.length < 1) return undefined;
     return [
-      { name: "Inter", data: regular, weight: 400, style: "normal" },
-      { name: "Inter", data: bold, weight: 800, style: "normal" },
+      { name: "Inter", data: inter[0], weight: 400, style: "normal" },
+      { name: "Inter", data: inter[1], weight: 800, style: "normal" },
+      { name: "Permanent Marker", data: marker[0], weight: 400, style: "normal" },
     ];
   } catch {
     return undefined;
@@ -44,8 +46,8 @@ async function loadInter(): Promise<LoadedFont[] | undefined> {
 export default async function OpengraphImage() {
   const { currentFilm } = getCurrentSchedule();
   const title = currentFilm.title;
-  const titleSize = title.length > 38 ? 58 : 84;
-  const fonts = await loadInter();
+  const titleSize = title.length > 38 ? 56 : 82;
+  const fonts = await loadFonts();
 
   return new ImageResponse(
     (
@@ -57,32 +59,52 @@ export default async function OpengraphImage() {
           flexDirection: "column",
           justifyContent: "space-between",
           background: "#000000",
-          padding: "76px 84px",
+          padding: "74px 84px",
           fontFamily: "Inter",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={OG_SEAL} width={84} height={84} alt="" />
           <div
             style={{
               display: "flex",
+              alignItems: "center",
+              marginLeft: 26,
+              fontFamily: "Permanent Marker",
+              fontSize: 54,
+              color: "#ffffff",
+            }}
+          >
+            <div style={{ display: "flex" }}>club</div>
+            <div
+              style={{
+                display: "flex",
+                width: 14,
+                height: 14,
+                marginLeft: 8,
+                marginRight: 8,
+                borderRadius: 999,
+                background: "#ff0033",
+              }}
+            />
+            <div style={{ display: "flex" }}>ciné</div>
+          </div>
+        </div>
+
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div
+            style={{
+              display: "flex",
+              marginBottom: 22,
               color: "#ff0033",
-              fontSize: 30,
+              fontSize: 28,
               fontWeight: 800,
               letterSpacing: 6,
             }}
           >
             {"À L'ANTENNE"}
           </div>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={OG_SEAL} width={120} height={120} alt="" />
-        </div>
-        <div style={{ display: "flex", flexDirection: "column" }}>
           <div
             style={{
               display: "flex",
@@ -98,25 +120,26 @@ export default async function OpengraphImage() {
           <div
             style={{
               display: "flex",
-              marginTop: 28,
+              marginTop: 26,
               color: "#aaaaaa",
-              fontSize: 32,
+              fontSize: 31,
               fontWeight: 400,
             }}
           >
             {`${currentFilm.director}, ${currentFilm.year}`}
           </div>
         </div>
+
         <div
           style={{
             display: "flex",
             color: "#8a8a8a",
-            fontSize: 24,
+            fontSize: 23,
             fontWeight: 400,
             letterSpacing: 4,
           }}
         >
-          CLUBCINE.XYZ · CHAÎNE 01
+          CLUBCINE.XYZ · CHANNEL 01 · 2026
         </div>
       </div>
     ),
